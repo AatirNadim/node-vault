1899c694940e9ec872f3935f17c153d7b9ad5581
AatirNadim <aatir.nadim@gmail.com>
2022-10-17 16:02:00 +0000

    arrow function without return

--- js-basics/first.js
+++ js-basics/first.js
@@ -3,3 +3,18 @@
 let a = 10
 a = 'happy'
 console.log(a)
+
+const summarize1 = () => {
+    return 'this is the arrow function'
+}
+
+function summarize(userName, userAge, userId) {
+    a = 122212
+    return `name --> ${userName}, age --> ${userAge}, id --> ${userId}`
+}
+console.log(summarize('aatir', 2333, 'aatir.nadim'))
+console.log(summarize1())
+const func = (a, b) => {return a + b;}
+const func1 = (a, b)=> ++a
+console.log(func(23,34))
+console.log(func1(21))
