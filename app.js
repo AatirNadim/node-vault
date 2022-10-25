b621cd09305ea8ba78687f840f1abd759a0a0187
AatirNadim <aatir.nadim@gmail.com>
2022-10-25 16:20:06 +0000

    serving sttatic pages in express

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const bodyParser = require('body-parser');
 // import express as a function
 const path = require('path')
 const adminRoutes = require('./routes/admin_route')
-const userRoutes = require('./routes/user_route')
+const userRoutes = require('./routes/user_route');
 const app = express()
 // here, we only import a function, which is why we can write createServer(routes), if an object, we would have said ,
 // createServer(routes.func)
 app.use(bodyParser.urlencoded({extended : false}));
 //parse bodies sent through the mail
-
-
+// console.log(__dirname)
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin',adminRoutes);
 app.use('/user', userRoutes);
 //this is for handling undefined path
